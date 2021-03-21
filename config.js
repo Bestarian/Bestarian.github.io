0b697cd4808f6227c9671937db8b882c70b46665
Kester Lukoschek <kester96@hotmail.de>
2021-03-21 22:27:39 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,44 @@
+var gmodLS = {}
+
+/**
+ * To get the users avatar and username, a steam web api key is needed.
+ * You can get it with your steam account for free here: https://steamcommunity.com/dev/apikey
+ * @type {String}
+ */
+gmodLS.steamWebApiKey = '853E83344C65E1F775EB7581702AC10A';
+
+/**
+ * A URI path or full URL to the loadingscreen background.
+ * Will be centered and streched if needed to the users resolution
+ * @type {String}
+ */
+gmodLS.backgroundImg  = 'https://files.facepunch.com/s/f3f82956055e.jpg';
+
+/**
+ * If you want music played in background add your music files here.
+ * The music files must be in OGG format to be played correctly.
+ * @type {Array}
+ */
+gmodLS.musicFiles = [
+];
+
+/**
+ * Music volume (Float value, max=1)
+ * 0   = off/no music
+ * 0.5 = 50% volume
+ * 1   = full volume
+ * @type {Number}
+ */
+gmodLS.musicVolume = 0;
+
+/**
+ * Serverrules - One rule per row
+ * @type {Array}
+ */
+gmodLS.rules = [
+    "Bei genügend 'pam_rtv' im chat gibt es einen Mapvote.",
+    "Dyingshot ist aktiviert, wenn man zielt und stirbt schießt man einmal sehr ungenau.",
+    "F1 zu den TTT2 Settings, jetzt geht Sprinten!!",
+    "Detective erhält 2 Credits je eigenem Traitor kill, nichts für den Assist",
+    "Karma Regeln nach dem Laden beachten!",
+];
